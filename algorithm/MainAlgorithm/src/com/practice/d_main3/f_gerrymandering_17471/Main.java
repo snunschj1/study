e049950559dcfc647b84b62bd56a6ef2258e329f
snunschj1 <snunschj1@gmail.com>
2020-01-15 14:10:57 +0900

    [main3] 게리맨더링 (17471) : 세번째 풀이(맞음) - 정점 조합으로 정점을 2개의 팀으로 나눈 뒤, 각 팀에 대해서 BFS를 통해 각 팀의 정점들이 서로 인접한지 확인한다.

--- algorithm/MainAlgorithm/src/com/practice/d_main3/f_gerrymandering_17471/Main.java
+++ algorithm/MainAlgorithm/src/com/practice/d_main3/f_gerrymandering_17471/Main.java
@@ -5,20 +5,18 @@ import java.io.*;
 
 public class Main {
 
-    private static final int CANCELED = 0;
+    private static final int TEAM1 = 1;
+    private static final int TEAM0 = 0;
+
     private static final int VISITED = 1;
-    private static final int CHECKED = 2;
+    private static final int NON_VISITED = 0;
 
     private static int N;
-
     private static LinkedList<Integer>[] list;
 
     private static int[] pop;
 
-    private static ArrayList<Pair> edges = new ArrayList<>();
-
     private static boolean flag = false;
-
     private static int answer = 1000;
 
     public static void main(String[] args) throws Exception {
@@ -48,12 +46,7 @@ public class Main {
             int k = s2i(st.nextToken());
 
             while (k-- > 0) {
-                int adjacentVertex = s2i(st.nextToken()) - 1;
-                list[i].add(adjacentVertex);
-
-                if (i < adjacentVertex) {
-                    edges.add(new Pair(i, adjacentVertex));
-                }
+                list[i].add(s2i(st.nextToken()) - 1);
             }
         }
 
@@ -61,10 +54,10 @@ public class Main {
     }
 
     private static void solve() {
-        for (int cnt = 1; cnt <= edges.size(); cnt++) {
-            int[] checkEdges = new int[edges.size()];
-            // Todo : 간선 조합을 구한다.
-            selectEdges(0, 0, cnt, checkEdges);
+        for (int cnt = 1; cnt < N; cnt++) {
+            int[] vertexes = new int[N];
+            // Todo : 정점 조합을 구한다.
+            selectVertexes(0, 0, cnt, vertexes);
         }
 
         if (flag) {
@@ -74,60 +67,61 @@ public class Main {
         }
     }
 
-    private static void selectEdges(int index, int cnt, int target, int[] checkEdges) {
+    private static void selectVertexes(int index, int cnt, int target, int[] vertexes) {
 
         if (cnt == target) {
-            // Todo : 간선 조합에 따라 정점들을 팀 0 / 팀 1 로 나눈다.
-            int[] vertexes = selectVertexes(checkEdges);
-            int[] tmpVertexes = new int[vertexes.length];
-            for (int i = 0; i < vertexes.length; i++) {
-                tmpVertexes[i] = vertexes[i];
-            }
 
-            int popOne = checkTeamOne(vertexes);
-            int popZero = checkTeamTwo(tmpVertexes);
+            // Todo : TEAM1, TEAM0 으로 나눠진 정점들이 각각 서로 인접하는지 확인한다.
+            int popOne = checkTeam(vertexes, cnt, TEAM1);
+            if (popOne != -1) {
+                int popZero = checkTeam(vertexes, N-cnt, TEAM0);
 
-            if (popOne != -1 && popZero != -1) {
-                int result = Math.abs(popOne - popZero);
+                if (popZero != -1) {
+                    int result = Math.abs(popOne - popZero);
 
-                if (!flag) {
-                    flag = true;
-                }
+                    if (!flag) {
+                        flag = true;
+                    }
 
-                if (answer > result) {
-                    answer = result;
+                    if (answer > result) {
+                        answer = result;
+                    }
                 }
             }
-
             return;
+
         } else {
-            if (index >= checkEdges.length) {
+            if (index >= N) {
                 return;
             }
         }
 
-        checkEdges[index] = VISITED;
-        selectEdges(index + 1, cnt + 1, target, checkEdges);
-        checkEdges[index] = CANCELED;
-        selectEdges(index + 1, cnt, target, checkEdges);
+        // Todo : 정점들을 팀 0 / 팀 1 로 나눈다.
+        vertexes[index] = TEAM1;
+        selectVertexes(index + 1, cnt + 1, target, vertexes);
+        vertexes[index] = TEAM0;
+        selectVertexes(index + 1, cnt, target, vertexes);
     }
 
-    private static int checkTeamOne(int[] vertexes) {
-        int[] c = new int[vertexes.length];
+    private static int checkTeam(int[] vertexes, int cnt, int team) {
+        int[] v = new int[N];
 
         int startIndex = 0;
 
-        for (int i = 0; i < vertexes.length; i++) {
-            if (vertexes[i] == VISITED) {
+        for (int i = 0; i < N; i++) {
+            if (vertexes[i] == team) {
                 startIndex = i;
                 break;
             }
         }
 
+        // Todo : 해당 팀으로 정해진 정점들이 인접한지 BFS로 확인한다.
         Queue<Integer> q = new LinkedList<>();
         q.add(startIndex);
-        vertexes[startIndex] = CHECKED;
-        c[startIndex] = VISITED;
+        v[startIndex] = VISITED;
+
+        int population = pop[startIndex];
+        int count = cnt - 1;
 
         while(!q.isEmpty()) {
             int i = q.remove();
@@ -135,108 +129,25 @@ public class Main {
             for (int j = 0; j < list[i].size(); j++) {
                 int next = list[i].get(j);
 
-                if (c[next] == 0) {
-                    if (vertexes[next] == VISITED) {
-                        vertexes[next] = CHECKED;
-                        c[next] = VISITED;
+                if (v[next] == NON_VISITED) {
+                    if (vertexes[next] == team) {
+                        v[next] = VISITED;
                         q.add(next);
+                        population += pop[next];
+                        --count;
                     }
                 }
             }
         }
-
-        int p = 0;
-
-        for (int i = 0; i < vertexes.length; i++) {
-            if (vertexes[i] == VISITED) {
-                p = -1;
-                break;
-            }
-
-            if (vertexes[i] == CHECKED) {
-                p += pop[i];
-            }
+        
+        if (count == 0) {
+            return population;
+        } else {
+            return -1;
         }
-
-        return p;
-    }
-
-    private static int checkTeamTwo(int[] vertexes) {
-        int[] c = new int[vertexes.length];
-
-        int startIndex = 0;
-
-        for (int i = 0; i < vertexes.length; i++) {
-            if (vertexes[i] == CANCELED) {
-                startIndex = i;
-                break;
-            }
-        }
-
-        Queue<Integer> q = new LinkedList<>();
-        q.add(startIndex);
-        vertexes[startIndex] = CHECKED;
-        c[startIndex] = VISITED;
-
-        while(!q.isEmpty()) {
-            int i = q.remove();
-
-            for (int j = 0; j < list[i].size(); j++) {
-                int next = list[i].get(j);
-
-                if (c[next] == 0) {
-                    if (vertexes[next] == CANCELED) {
-                        vertexes[next] = CHECKED;
-                        c[next] = VISITED;
-                        q.add(next);
-                    }
-                }
-            }
-        }
-
-        int p = 0;
-
-        for (int i = 0; i < vertexes.length; i++) {
-            if (vertexes[i] == CANCELED) {
-                p = -1;
-                break;
-            }
-
-            if (vertexes[i] == CHECKED) {
-                p += pop[i];
-            }
-        }
-
-        return p;
-    }
-
-    private static int[] selectVertexes(int[] checkEdges) {
-        int[] vertexes = new int[N];
-
-        for (int i = 0; i < checkEdges.length; i++) {
-            if (checkEdges[i] == VISITED) {
-                Pair p = edges.get(i);
-                int x = p.x;
-                int y = p.y;
-
-                vertexes[x] = VISITED;
-                vertexes[y] = VISITED;
-            }
-        }
-
-        return vertexes;
     }
 
     private static int s2i(String s) {
         return Integer.parseInt(s);
-    }
-}
-
-class Pair {
-    int x, y;
-
-    Pair(int x, int y) {
-        this.x = x;
-        this.y = y;
     }
 }
