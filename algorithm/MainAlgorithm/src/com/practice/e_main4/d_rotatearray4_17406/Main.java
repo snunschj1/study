094e3ea93f7e8e698b09ff7c2e5bfca0f4b4a974
snunschj1 <snunschj1@gmail.com>
2020-01-20 19:50:57 +0900

    [main4] 배열돌리기4(17406) : 첫 풀이(맞음) - 1) 회전 연산 순서 = next_permutation 2) dfs로 배열 회전 시키기 3) 한 번의 연산에서 외곽 한 줄씩 끝날 때마다 startRow, startCol은 +1, finRow, finCol은 -1 을 해서 만날 때까지 한다. 3) 주의할 점, 다음 순열마다 처음 default 배열은 입력받은 그 값이어야 한다. 깊은 복사를 해두자.

--- algorithm/MainAlgorithm/src/com/practice/e_main4/d_rotatearray4_17406/Main.java
+++ algorithm/MainAlgorithm/src/com/practice/e_main4/d_rotatearray4_17406/Main.java
@@ -14,7 +14,9 @@ public class Main {
 
     private static int[][] a;
 
-    private static ArrayDeque<Pair> operates = new ArrayDeque<>();
+    private static Pair[] operates;
+
+    private static int answer = 5000;
 
 
     public static void main(String[] args) throws Exception {
@@ -31,6 +33,7 @@ public class Main {
         K = s2i(st.nextToken());
 
         a = new int[N][M];
+        operates = new Pair[K];
 
         for (int r = 0; r < N; r++) {
             st = new StringTokenizer(br.readLine());
@@ -47,28 +50,56 @@ public class Main {
             int c = s2i(st.nextToken()) - 1;
             int s = s2i(st.nextToken());
 
-            operates.add(new Pair(r, c, s));
+            operates[k] = new Pair(r, c, s);
         }
     }
 
     private static void solve() {
 
-        while (!operates.isEmpty()) {
-            Pair p = operates.pop();
+        int[] order = new int[K];
+        for (int k = 0; k < K; k++) {
+            order[k] = k;
+        }
 
-            findRowAndCol(p);
+        do {
 
-            while (startRow != finRow) {
-                dfs(startRow, startCol, 0, 0);
+            int[][] copy = copyArr(a);
 
-                startRow += 1;
-                startCol += 1;
-                finRow -= 1;
-                finCol -= 1;
+            for (int i = 0; i < K; i++) {
+
+                Pair p = operates[order[i]];
+
+                findRowAndCol(p);
+
+                while (startRow != finRow) {
+                    dfs(startRow, startCol, 0, 0, copy);
+
+                    startRow += 1;
+                    startCol += 1;
+                    finRow -= 1;
+                    finCol -= 1;
+                }
+            }
+
+            int tmpMin = calculateMin(copy);
+
+            if (answer > tmpMin) answer = tmpMin;
+
+        } while (next_permutation(order));
+
+        System.out.println(answer);
+    }
+
+    private static int[][] copyArr(int[][] a) {
+        int[][] copy = new int[N][M];
+
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < M; c++) {
+                copy[r][c] = a[r][c];
             }
         }
 
-        System.out.println(calculateMin());
+        return copy;
     }
 
     private static void findRowAndCol(Pair p) {
@@ -78,44 +109,79 @@ public class Main {
         finCol = p.c + p.s;
     }
 
-    private static void dfs(int row, int col, int dir, int preValue) {
+    private static void dfs(int row, int col, int dir, int preValue, int[][] copy) {
         if (row == startRow && col == startCol && dir == 3) {
-            a[row][col] = preValue;
+            copy[row][col] = preValue;
             return;
         }
 
         /** 다음 dfs 인자 */
         int nr = row + dr[dir];
         int nc = col + dc[dir];
-        int curValue = a[row][col];
+        int curValue = copy[row][col];
 
         if (row == startRow && col == startCol && preValue == 0) {
-            dfs(nr, nc, dir, curValue);
+            dfs(nr, nc, dir, curValue, copy);
         } else {
 
             /** 이전 값을 배열의 현재 원소에 넣어준다. */
-            a[row][col] = preValue;
+            copy[row][col] = preValue;
 
             if (nr < startRow || nr > finRow || nc < startCol || nc > finCol) {
                 /** 범위를 넘어가는 경우 방향을 바꿔준다. */
                 nr = row + dr[dir + 1];
                 nc = col + dc[dir + 1];
 
-                dfs(nr, nc, dir + 1, curValue);
+                dfs(nr, nc, dir + 1, curValue, copy);
             } else {
                 /** 다음 dfs  */
-                dfs(nr, nc, dir, curValue);
+                dfs(nr, nc, dir, curValue, copy);
             }
         }
     }
 
-    private static int calculateMin() {
-        int min = Integer.MAX_VALUE;
+    private static boolean next_permutation(int[] a) {
+        int i = a.length - 1;
+
+        while (i > 0 && a[i] <= a[i-1]) {
+            i -= 1;
+        }
+
+        if (i <= 0) {
+            return false;
+        }
+
+        int j = a.length - 1;
+
+        while (a[j] <= a[i-1]) {
+            j -= 1;
+        }
+
+        int tmp = a[i-1];
+        a[i-1] = a[j];
+        a[j] = tmp;
+
+        j = a.length - 1;
+
+        while (i < j) {
+            tmp = a[i];
+            a[i] = a[j];
+            a[j] = tmp;
+
+            i += 1;
+            j -= 1;
+        }
+
+        return true;
+    }
+
+    private static int calculateMin(int[][] copy) {
+        int min = 5000;
 
         for (int r = 0; r < N; r++) {
             int sum = 0;
             for (int c = 0; c < M; c++) {
-                sum += a[r][c];
+                sum += copy[r][c];
             }
 
             if (min > sum) min = sum;
