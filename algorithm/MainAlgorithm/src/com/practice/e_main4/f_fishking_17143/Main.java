9fe8dad9bafe192758de9045cd6de49d1ed18f45
snunschj1 <snunschj1@gmail.com>
2020-01-22 01:13:16 +0900

    [main4] 낚시왕(17143) : 세번째 풀이(맞음) - 1) R*C 배열에 상어(속도, 방향, 크기) 정보가 있다 2) 몫과 나머지를 활용해서 다음 방향과 다음 좌표를 구한다 3) 이번 차례 열(낚시왕이 낚시할 열)에 존재할 수 있는 가장 최상단 row를 상어 움직이는 과정에서 구해서 낚시를 한다

--- algorithm/MainAlgorithm/src/com/practice/e_main4/f_fishking_17143/Main.java
+++ algorithm/MainAlgorithm/src/com/practice/e_main4/f_fishking_17143/Main.java
@@ -5,14 +5,13 @@ import java.io.*;
 
 public class Main {
 
-    private static int[] dr = {-1, 1, 0, 0};
-    private static int[] dc = {0, 0, 1, -1};
-
     private static int R, C, M;
 
     private static Shark[][] map;
 
     private static int answer = 0;
+
+    private static int minRow;
 
     public static void main(String[] args) throws Exception {
         inputData();
@@ -28,15 +27,22 @@ public class Main {
         M = s2i(st.nextToken());
 
         map = new Shark[R][C];
+        minRow = R;
 
         for (int m = 0; m < M; m++) {
             st = new StringTokenizer(br.readLine());
 
             int r = s2i(st.nextToken()) - 1;
             int c = s2i(st.nextToken()) - 1;
-            int s = s2i(st.nextToken());
-            int d = s2i(st.nextToken()) - 1;
+            int s = s2i(st.nextToken()) - 1;
+            int d = s2i(st.nextToken());
             int z = s2i(st.nextToken());
+
+            if (c == 0) {
+                if (minRow > r) {
+                    minRow = r;
+                }
+            }
 
             Shark shark = new Shark(s, d, z);
 
@@ -45,68 +51,135 @@ public class Main {
     }
 
     private static void solve() {
-        for (int i = 0; i < C; i++) {
+        catchShark(0);
+        for (int i = 1; i < C; i++) {
+            minRow = R;
+            moveShark(i);
             catchShark(i);
-            moveShark();
         }
 
         System.out.println(answer);
     }
 
     private static void catchShark(int col) {
-        for (int r = 0; r < R; r++) {
-            if (map[r][col] != null) {
-                answer += map[r][col].z;
-                map[r][col] = null;
-                break;
-            }
+        if (minRow < R) {
+            answer += map[minRow][col].z;
+            map[minRow][col] = null;
         }
     }
 
-    private static void moveShark() {
+    private static void moveShark(int col) {
 
         Shark[][] tmp = new Shark[R][C];
 
-        for (int r = 0; r < R; r++) {
-            for (int c = 0; c < C; c++) {
-                if (map[r][c] != null) {
-                    int s = map[r][c].s;
-                    int d = map[r][c].d;
-                    int z = map[r][c].z;
-                    go(r, c, s, d, z, s, tmp);
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (map[i][j] != null) {
+                    int r = i;
+                    int c = j;
+                    int s = map[i][j].s;
+                    int d = map[i][j].d;
+                    int z = map[i][j].z;
+
+                    int nr, nc, nd, ns;
+
+                    int a, b;
+
+                    if (s == -1) {
+                        nr = r;
+                        nc = c;
+                        nd = d;
+                        ns = s;
+
+                    } else {
+                        if (d % 2 == 0) {
+                            if (d == 2) {
+                                ns = s + r;
+
+                                a = ns / (R - 1);
+                                b = ns % (R - 1);
+
+                                if (a % 2 == 0) {
+                                    nd = d;
+                                    nr = (b + 1);
+                                    nc = c;
+                                } else /* if (a % 2 == 1) */ {
+                                    nd = 1;
+                                    nr = (R - 2 - b);
+                                    nc = c;
+                                }
+
+                            } else /* if (d == 4) */ {
+                                ns = s + (C - 1 - c);
+
+                                a = ns / (C - 1);
+                                b = ns % (C - 1);
+
+                                if (a % 2 == 0) {
+                                    nd = d;
+                                    nr = r;
+                                    nc = (C - 2 - b);
+                                } else /* if (a % 2 == 1) */ {
+                                    nd = 3;
+                                    nr = r;
+                                    nc = (b + 1);
+                                }
+                            }
+                        } else /* if (d % 2 == 1) */ {
+                            if (d == 1) {
+                                ns = s + (R - 1 - r);
+
+                                a = ns / (R - 1);
+                                b = ns % (R - 1);
+
+                                if (a % 2 == 0) {
+                                    nd = d;
+                                    nr = (R - 2 - b);
+                                    nc = c;
+                                } else /* if (a % 2 == 1) */ {
+                                    nd = 2;
+                                    nr = (b + 1);
+                                    nc = c;
+                                }
+
+                            } else /* if (d == 3) */ {
+                                ns = s + c;
+
+                                a = ns / (C - 1);
+                                b = ns % (C - 1);
+
+                                if (a % 2 == 0) {
+                                    nd = d;
+                                    nr = r;
+                                    nc = (b + 1);
+                                } else /* if (a % 2 == 1) */ {
+                                    nd = 4;
+                                    nr = r;
+                                    nc = (C - 2 - b);
+                                }
+                            }
+                        }
+                        ns = s;
+                    }
+
+                    if (tmp[nr][nc] != null) {
+                        if (z < tmp[nr][nc].z) {
+                            continue;
+                        }
+                    }
+
+                    if (nc == col) {
+                        if (minRow > nr) {
+                            minRow = nr;
+                        }
+                    }
+
+                    tmp[nr][nc] = new Shark(ns, nd, z);
                 }
             }
         }
 
         map = tmp;
-    }
-
-    private static void go(int r, int c, int s, int d, int z, int cnt, Shark[][] tmp) {
-        if (cnt == 0) {
-            Shark other = tmp[r][c];
-            if (other != null) {
-                if (z < other.z) {
-                    return;
-                }
-            }
-
-            tmp[r][c] = new Shark(s, d, z);
-            return;
-        }
-
-        int nr = r + dr[d];
-        int nc = c + dc[d];
-        int nd = d;
-
-        if (0 > nr || R <= nr || 0 > nc || C <= nc) {
-            if (d % 2 == 0) nd += 1;
-            else /* if (d % 2 == 1) */ nd -= 1;
-
-            nr = r + dr[nd];
-            nc = c + dc[nd];
-        }
-
-        go(nr, nc, s, nd, z, cnt - 1, tmp);
     }
 
     private static int s2i(String s) {
