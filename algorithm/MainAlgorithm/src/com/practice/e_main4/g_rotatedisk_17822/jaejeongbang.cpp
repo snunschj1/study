12e39c668bff881ea7f84441c225e51c5202b9df
bggbr <jaejung603@gmail.com>
2020-01-25 21:55:22 +0900

    jaejeongbang class

---
+++ algorithm/MainAlgorithm/src/com/practice/e_main4/g_rotatedisk_17822/jaejeongbang.cpp
@@ -0,0 +1,5 @@
+//
+// Created by apple on 2020/01/25.
+//
+
+#include "jaejeongbang.h"
---
+++ algorithm/MainAlgorithm/src/com/practice/e_main4/g_rotatedisk_17822/jaejeongbang.h
@@ -0,0 +1,14 @@
+//
+// Created by apple on 2020/01/25.
+//
+
+#ifndef MAINALGORITHM_JAEJEONGBANG_H
+#define MAINALGORITHM_JAEJEONGBANG_H
+
+
+class jaejeongbang {
+
+};
+
+
+#endif //MAINALGORITHM_JAEJEONGBANG_H
