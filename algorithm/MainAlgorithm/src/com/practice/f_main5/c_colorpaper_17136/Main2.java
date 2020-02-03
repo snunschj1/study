35c3d6c1a5598644b8c1b8f991ee595e74b560bb
snunschj1 <snunschj1@gmail.com>
2020-02-03 14:31:34 +0900

    [main5] 색종이 붙이기(17136) : 풀이 2(맞음) - 1) 1을 모두 커버할 수 있는 각 색종이 개수 조합을 구한다 2) 크기 5 색종이부터 가능한 위치 조합을 구하고, 각 색종이의 개수 만큼 위치를 모두 결정했다면, 다음 색종이로 넘어간다.

--- algorithm/MainAlgorithm/src/com/practice/f_main5/c_colorpaper_17136/Main2.java
+++ algorithm/MainAlgorithm/src/com/practice/f_main5/c_colorpaper_17136/Main2.java
@@ -5,12 +5,14 @@ import java.io.*;
 
 public class Main2 {
 
-    private static int remainOne;
+    private static final int TARGET = 1;
+
+    private static int oneCnt;
 
     private static int[][] map = new int[10][10];
     private static int[][] tMap;
 
-    private static int[] paperOrder = {1, 2, 3, 4, 5};
+    private static int[] paperCount = new int[6];
 
     private static int answer = 25;
 
@@ -29,7 +31,7 @@ public class Main2 {
                 map[r][c] = s2i(st.nextToken());
 
                 if (map[r][c] == 1) {
-                    remainOne++;
+                    oneCnt++;
                 }
             }
         }
@@ -37,81 +39,97 @@ public class Main2 {
     }
 
     private static void solve() {
-        int tRemainOne;
-        int[] paperCnt;
-
-        do {
-            // Todo : test
-            Test.printStr("===START===\n");
-            Test.printArr(paperOrder);
-
-            tMap = copyMap(map);
-            tRemainOne = remainOne;
-            paperCnt = new int[]{0, 5, 5, 5, 5, 5};
-
-            for (int i = 0; i < 5; i++) {
-                int usedPaperCnt = attachPaper(paperOrder[i], paperCnt);
-
-                tRemainOne -= usedPaperCnt;
-
-                // Todo : test
-                Test.printMap(tMap);
-                Test.printStr("removeOne = " + usedPaperCnt + "\n");
-                Test.printStr("remainOne = " + tRemainOne + "\n");
-            }
-
-            if (tRemainOne == 0) {
-                // Todo : test
-                Test.printStr("SUCCESS\n");
-
-                int usedPaper = countUsedPaper(paperCnt);
-
-                if (answer > usedPaper) {
-                    answer = usedPaper;
-                }
-            }
-        } while (next_permutation(paperOrder));
+        decidePaperCount(1);
 
         if (answer == 25) {
             System.out.println("-1");
         } else {
             System.out.println(answer);
         }
-
-        // Todo : test
-        Test.flush();
     }
 
+    private static void decidePaperCount(int index) {
+        // 각 색종이마다 몇 개를 배당할지 정하는 재귀식
+        if (index > 5) {
 
-    private static int attachPaper(int paperSize, int[] paperCnt) {
-        int sum = 0;
+            if (checkPaperCount()) {
+                // 색종이 개수 조합이 1을 모두 cover 할 수 있는 경우
+
+                if (countPaper() > answer) {
+                    // 색종이 개수가 최소값보다 크다면 건너띔
+                    return;
+                }
+
+                tMap = copyMap(map);
+
+                // 색종이 크기 5 부터 색종이 위치를 결정한다.
+                int target = paperCount[5];
+                paperCombination(0, target, 5, tMap);
+            }
+
+            return;
+        }
+
+        for (int i = 0; i <= 5; i++) {
+            paperCount[index] = i;
+            decidePaperCount(index + 1);
+        }
+
+    }
+
+    private static void paperCombination(int cnt, int target, int paperSize, int[][] map) {
+        // 해당 색종이 개수만큼 색종이 위치 조합을 만듦
+
+        if (cnt >= target) {
+
+            if (paperSize <= 1) {
+                // 색종이 크기 1까지 모두 위치 조합을 완성하면, 사용한 색종이가 몇 개인지 계산
+                int usedPaper = countPaper();
+
+                if (answer > usedPaper) {
+                    answer = usedPaper;
+                }
+                return;
+            }
+
+            // 해당 색종이의 개수 만큼 위치를 결정했다면, 다음 색종이로 넘어간다.
+            int nextTarget = paperCount[paperSize - 1];
+            paperCombination(0, nextTarget, paperSize - 1, map);
+            return;
+        }
+
 
         for (int r = 0; r <= 10 - paperSize; r++) {
             for (int c = 0; c <= 10 - paperSize; c++) {
 
-                if (paperCnt[paperSize] == 0) {
-                    break;
-                }
+                if (map[r][c] != TARGET) continue;
 
-                if (Paper.checkPaper(r, c, paperSize, tMap)) {
-                    Paper.attachPaper(r, c, paperSize, tMap);
-                    sum += (paperSize * paperSize);
-                    paperCnt[paperSize] -= 1;
+                if (Paper.checkPaper(r, c, paperSize, map)) {
+                    // 색종이를 붙일 수 있는 경우
+                    Paper.attachPaper(r, c, paperSize, map);
+                    paperCombination(cnt + 1, target, paperSize, map);
+                    Paper.detachPaper(r, c, paperSize, map);
                 }
             }
         }
-
-        return sum;
     }
 
-    private static int countUsedPaper(int[] paperCnt) {
+    private static boolean checkPaperCount() {
         int sum = 0;
 
         for (int i = 1; i <= 5; i++) {
-            sum += paperCnt[i];
+            sum += (i * i * paperCount[i]);
         }
+        return sum == oneCnt;
+    }
 
-        return 25 - sum;
+    private static int countPaper() {
+        int sum = 0;
+
+        for (int i = 1; i <= 5; i++) {
+            sum += paperCount[i];
+        }
+        return sum;
     }
 
     private static int s2i(String s) {
@@ -128,41 +146,6 @@ public class Main2 {
         }
         return tmp;
     }
-
-    private static boolean next_permutation(int[] a) {
-        int i = a.length - 1;
-
-        while (i > 0 && a[i] <= a[i - 1]) {
-            i -= 1;
-        }
-
-        if (i <= 0) {
-            return false;
-        }
-
-        int j = a.length - 1;
-
-        while (a[j] <= a[i - 1]) {
-            j -= 1;
-        }
-
-        int tmp = a[j];
-        a[j] = a[i - 1];
-        a[i - 1] = tmp;
-
-        j = a.length - 1;
-
-        while (i < j) {
-            tmp = a[i];
-            a[i] = a[j];
-            a[j] = tmp;
-
-            i += 1;
-            j -= 1;
-        }
-
-        return true;
-    }
 }
 
 class Paper {
@@ -172,6 +155,7 @@ class Paper {
     private static final int ATTACH = 2;
 
     static boolean checkPaper(int row, int col, int paperSize, int[][] map) {
+        // 색종이를 붙일 수 있는지 본다.
         for (int r = row; r < row + paperSize; r++) {
             for (int c = col; c < col + paperSize; c++) {
                 if (map[r][c] == EMPTY || map[r][c] == ATTACH) {
@@ -183,10 +167,18 @@ class Paper {
     }
 
     static void attachPaper(int row, int col, int paperSize, int[][] map) {
+        setPaper(row, col, paperSize, map, TARGET, ATTACH);
+    }
+
+    static void detachPaper(int row, int col, int paperSize, int[][] map) {
+        setPaper(row, col, paperSize, map, ATTACH, TARGET);
+    }
+
+    private static void setPaper(int row, int col, int paperSize, int[][] map, int current, int change) {
         for (int r = row; r < row + paperSize; r++) {
             for (int c = col; c < col + paperSize; c++) {
-                if (map[r][c] == TARGET) {
-                    map[r][c] = ATTACH;
+                if (map[r][c] == current) {
+                    map[r][c] = change;
                 }
             }
         }
