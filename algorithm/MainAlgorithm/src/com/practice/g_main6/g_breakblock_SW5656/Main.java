33cab60ba8a4ef78698768a4934e3804edd09252
snunschj1 <snunschj1@gmail.com>
2020-02-12 17:21:28 +0900

    [main6] 벽돌깨기 (SW 5656) : 풀이 1(맞음, 개선) - 풀이 2와 가장 큰 차이는 1) 조합을 완성시키고 조합에 따라서 구슬을 쏠지 2) DFS를 돌면서 map 정보를 계속 변경을 줄 것인지 -> 2) 가 훨씬 빠르다. memoization 의 문제, 예를 들어, 0열 1열 2열 순서대로 구슬을 쏜다면, 2)는 2열에 쏠 때 0열 -> 1열 구슬 쏜 것을 다시 반복해볼 필요가 없는데, 1) 은다시 반복해서 구하게 된다.

--- algorithm/MainAlgorithm/src/com/practice/g_main6/g_breakblock_SW5656/Main.java
+++ algorithm/MainAlgorithm/src/com/practice/g_main6/g_breakblock_SW5656/Main.java
@@ -11,9 +11,7 @@ public class Main {
     private static int T, N, W, H;
 
     private static int[][] map;
-    private static int[][] tMap;    // 각 조합마다 사용할 map
 
-    private static int[] combination;
     private static Queue<Block> bombs;      // 한 번 구슬 쏠 때마다, 첫 파괴될 블록과 그로 인해서 연쇄적으로 폭파되는 블록 중 숫자가 2 이상인 것을 담는 큐
 
     private static int brokenBlocks;
@@ -60,68 +58,55 @@ public class Main {
 
     private static void solve(int test) {
         answer = Integer.MAX_VALUE;
-        combination = new int[N];
-
-        makeCombination(0);
+        makeCombination(0, map, totalBlocks);
         System.out.printf("#%d %d\n", test, answer);
     }
 
-    private static void makeCombination(int cnt) {
-        // Todo : N 번 구슬을 몇 열에 쏠지 조합을 정한다.
+    private static void makeCombination(int cnt, int[][] map, int remainBlocks) {
 
-        if (cnt >= N) {
-            tMap = copyMap(map);
-            brokenBlocks = 0;
-
-            startGame();
+        if (cnt >= N) {         // Todo : 구슬 N 번 쏘기 완료
+            if (answer > remainBlocks) {
+                answer = remainBlocks;
+            }
             return;
         }
 
-        for (int i = 0; i < W; i++) {
-            combination[cnt] = i;
-            makeCombination(cnt + 1);
+        for (int c = 0; c < W; c++) {       // Todo : 각 열에 구슬 쏴보기
+            int[][] tMap = copyMap(map);
+
+            int remain = startGame(tMap, c, remainBlocks);
+            makeCombination(cnt + 1, tMap, remain);
         }
     }
 
-    private static void startGame() {
-        for (int n = 0; n < N; n++) {
-            // Todo : N 번 구슬을 쏜다
+    private static int startGame(int[][] map, int col, int remainBlocks) {
+        brokenBlocks = 0;
 
-            int start = combination[n];     // 구슬을 쏘는 열
+        for (int r = 0; r < H; r++) {
 
-            for (int r = 0; r < H; r++) {
+            if (map[r][col] != 0) {     // Todo : 해당 열의 가장 위의 블록
 
-                if (tMap[r][start] != 0) {
-                    // Todo : 해당 열의 가장 위의 블록
-
-                    breakBlock(new Block(r, start, tMap[r][start]));
-                    moveRemainBlocks();
-                    break;
-                }
+                breakBlock(new Block(r, col, map[r][col]), map);
+                moveRemainBlocks(map);
+                break;
             }
         }
-
-        if (answer > (totalBlocks - brokenBlocks)) {
-            answer = totalBlocks - brokenBlocks;
-        }
+        return remainBlocks - brokenBlocks;
     }
 
-    private static void breakBlock(Block start) {
+    private static void breakBlock(Block start, int[][] map) {
         bombs = new LinkedList<>();
         bombs.add(start);
-        tMap[start.r][start.c] = 0;
+        map[start.r][start.c] = 0;
         brokenBlocks++;
 
-        while (!bombs.isEmpty()) {
-            // Todo : 블록 파괴가 끝날 때까지 진행
-
+        while (!bombs.isEmpty()) {      // Todo : 블록 파괴가 끝날 때까지 진행
             Block broken = bombs.remove();
-            explode(broken);
+            explode(broken, map);
         }
     }
 
-    private static void explode(Block block) {
-        // Todo : 상하좌우로 폭발하는 로직 처리
+    private static void explode(Block block, int[][] map) {     // Todo : 상하좌우로 폭발하는 로직 처리
 
         int nr;
         int nc;
@@ -136,35 +121,34 @@ public class Main {
                 nc += dc[k];
 
                 if (0 > nr || nr >= H || 0 > nc || nc >= W) break; // 범위 밖
-                if (tMap[nr][nc] == 0) continue;  // 블록이 없거나 이미 파괴된 경우
+                if (map[nr][nc] == 0) continue;  // 블록이 없거나 이미 파괴된 경우
 
-                if (tMap[nr][nc] == 1) { // 블록의 숫자가 1인 경우, visit 처리와 brokenBlocks++ 만 한다.
-                    tMap[nr][nc] = 0;
+                if (map[nr][nc] == 1) { // 블록의 숫자가 1인 경우, visit 처리와 brokenBlocks++ 만 한다.
+                    map[nr][nc] = 0;
                     brokenBlocks++;
                 } else {
-                    bombs.add(new Block(nr, nc, tMap[nr][nc])); // 그 외의 숫자는 연쇄작용이 일어나므로, 큐에 추가한다.
-                    tMap[nr][nc] = 0;
+                    bombs.add(new Block(nr, nc, map[nr][nc])); // 그 외의 숫자는 연쇄작용이 일어나므로, 큐에 추가한다.
+                    map[nr][nc] = 0;
                     brokenBlocks++;
                 }
             }
         }
     }
 
-    private static void moveRemainBlocks() {
+    private static void moveRemainBlocks(int[][] map) {
 
         for (int c = 0; c < W; c++) {
 
             int index = H - 1;
 
-            for (int r = H - 1; r >= 0; r--) {  // Todo : 열의 마지막 행부터 0행까지 검토
-                if (tMap[r][c] != 0) {
-                    tMap[index--][c] = tMap[r][c];
+            for (int r = H - 1; r >= 0; r--) {      // Todo : 열의 마지막 행부터 0행까지 검토
+                if (map[r][c] != 0) {
+                    map[index--][c] = map[r][c];
                 }
             }
 
-            for (int r = 0; r <= index; r++) {
-                // Todo : 남아있는 블록을 다 채운 뒤, 나머지는 0으로 채운다.
-                tMap[r][c] = 0;
+            for (int r = 0; r <= index; r++) {      // Todo : 남아있는 블록을 다 채운 뒤, 나머지는 0으로 채운다.
+                map[r][c] = 0;
             }
         }
     }
